b7c60e8d9f70002573ba263e184f737d54f52ca1
Shyla Subrahmanya <shylasubrahmanya@gmail.com>
2023-02-11 12:01:42 -0800

    Login Page

---
+++ sharideapp/lib/LoginScreen.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+Widget builEmail() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        'Email',
+        style: TextStyle(
+            color: Color.fromARGB(255, 239, 238, 238),
+            fontSize: 16,
+            fontWeight: FontWeight.bold),
+      ),
+      SizedBox(height: 10),
+      Container(
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black26, blurRadius: 6, offset: Offset(2, 2))
+            ]),
+        height: 60,
+        child: TextField(
+          keyboardType: TextInputType.emailAddress,
+          style: TextStyle(color: Color.fromARGB(221, 15, 15, 15)),
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.only(top: 14),
+            prefixIcon: Icon(
+              Icons.email,
+              color: Color.fromARGB(255, 15, 16, 15),
+            ),
+            hintText: 'Email',
+            hintStyle: TextStyle(color: Colors.black38),
+          ),
+        ),
+      )
+    ],
+  );
+}
+
+Widget builPassword() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        'Password',
+        style: TextStyle(
+            color: Color.fromARGB(255, 12, 12, 12),
+            fontSize: 16,
+            fontWeight: FontWeight.bold),
+      ),
+      SizedBox(height: 10),
+      Container(
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black26, blurRadius: 6, offset: Offset(2, 2))
+            ]),
+        height: 60,
+        child: TextField(
+          obscureText: true,
+          style: TextStyle(color: Color.fromARGB(255, 14, 13, 13)),
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.only(top: 14),
+            prefixIcon: Icon(
+              Icons.lock,
+              color: Color.fromARGB(255, 14, 15, 14),
+            ),
+            hintText: 'Password',
+            hintStyle: TextStyle(color: Colors.black38),
+          ),
+        ),
+      )
+    ],
+  );
+}
+
+Widget buildForgotPassword() {
+  return Container(
+    alignment: Alignment.centerRight,
+    child: TextButton(
+        onPressed: () => print('Forgot Password Pressed'),
+        //padding: EdgeInsets.only(right: 0),
+        child: Text(
+          'Forgot Password?',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        )),
+  );
+}
+
+Widget buildLoginBtn() {
+  return Container(
+      padding: EdgeInsets.symmetric(vertical: 25),
+      width: double.infinity,
+      child: ElevatedButton(
+        child: Text('Sign In'),
+        style: ElevatedButton.styleFrom(
+          primary: Color.fromARGB(255, 19, 86, 209),
+          onPrimary: Colors.white,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          textStyle: TextStyle(
+              color: Colors.black, fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+        onPressed: () {
+          print('Pressed');
+        },
+      ));
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                      Colors.black,
+                      Colors.black,
+                      Colors.black,
+                      Colors.black,
+                    ])),
+                child: SingleChildScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  padding: EdgeInsets.symmetric(horizontal: 25, vertical: 120),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Welcome to ShareRide \n ',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          //fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      Text(
+                        'Sign In',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 50),
+                      builEmail(),
+                      SizedBox(height: 20),
+                      builPassword(),
+                      buildForgotPassword(),
+                      buildLoginBtn(),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
