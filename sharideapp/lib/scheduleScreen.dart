919b802c0574334917d1b25fa76442edbb1e11c1
Daniel Lee <daniellee9707@gmail.com>
2023-03-05 22:24:25 -0800

    added schedule screen

---
+++ sharideapp/lib/scheduleScreen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:intl/intl.dart';
+
+class ScheduleScreen extends StatefulWidget {
+
+  @override
+  State<ScheduleScreen> createState() => _ScheduleState();
+}
+
+class _ScheduleState extends State<ScheduleScreen> {
+   DateTime scheduledDate=DateTime.now();
+
+  void _setSchedule(DateTime date) {
+
+    setState(() {
+      scheduledDate = date;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sharide'),
+      ),
+      body: Container(
+          child: Column(
+        children: [
+          Text('Select your ride schedule date and time:', style:  TextStyle(color:Colors.black, fontSize: 18, fontWeight: FontWeight.bold),),
+          ElevatedButton(
+            child: Text('Select date/time for your next ride'),
+            onPressed: () {
+              DatePicker.showDatePicker(context,
+                  showTitleActions: true,
+                  minTime: DateTime.now(),
+                  maxTime: DateTime(2030, 12, 31), onChanged: (date) {
+                print('change $date');
+              }, onConfirm: (date) {
+                _setSchedule(date);
+              }, currentTime: DateTime.now(), locale: LocaleType.en);
+            },
+          ),
+          Text('Selected date: ' + DateFormat.yMMMd().format(scheduledDate)),
+          ElevatedButton(onPressed: (){}, child: Text('Confirm')),
+        ],
+      )),
+    );
+  }}
