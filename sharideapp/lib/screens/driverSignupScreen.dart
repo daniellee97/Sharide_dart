583a8f8de87393048eb3aaa6169f24db61b16c20
Daniel Lee <daniellee9707@gmail.com>
2023-03-30 21:25:50 -0700

    Applied state managements for sign up screens

--- sharideapp/lib/screens/driverSignupScreen.dart
+++ sharideapp/lib/screens/driverSignupScreen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:http/http.dart' as http;
 
 class SignUpFormDriver extends StatefulWidget {
   @override
@@ -6,15 +9,33 @@ class SignUpFormDriver extends StatefulWidget {
 }
 
 class _DriverSignUpFormState extends State<SignUpFormDriver> {
+  AlertDialog showSignUpSuccessfully = const AlertDialog(
+      title: Text("Sign up successfully, please go back and log in"),
+      actions: [
+        // okButton,
+      ]);
+
+  AlertDialog showSignUpUnsuccessfully =
+      const AlertDialog(title: Text("Sign up unsuccessfully"), actions: [
+    // okButton,
+  ]);
+
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _vehicleYearController = TextEditingController();
+  final TextEditingController _vehicleMakeController = TextEditingController();
+  final TextEditingController _licenceController = TextEditingController();
+  // final TextEditingController _vehicleModelController = TextEditingController();
+
   String? _name;
   String? _email;
   String? _password;
   int? _vehicleYear;
   String? _vehicleMake;
   String? _licence;
-  String? _vehicleModel;
+  // String? _vehicleModel;
 
   //Sign up page for driver( vehicle year, make, model, plate number)
 
@@ -24,7 +45,13 @@ class _DriverSignUpFormState extends State<SignUpFormDriver> {
       return;
     }
     setState(() {
+      _name = _nameController.text;
       _email = _emailController.text;
+      _password = _passwordController.text;
+      _vehicleYear = int.tryParse(_vehicleYearController.text);
+      _vehicleMake = _vehicleMakeController.text;
+      // _vehicleModel = _vehicleModelController.text;
+      _licence = _licenceController.text;
     });
   }
 
@@ -38,10 +65,62 @@ class _DriverSignUpFormState extends State<SignUpFormDriver> {
     });
   }
 
+  void _signUpDriver() {
+    // String? _name;
+    // String? _email;
+    // String? _password;
+    // int? _vehicleYear;
+    // String? _vehicleMake;
+    // String? _licence;
+    // String? _vehicleModel;
+
+    var body = {
+      'license_no': _licence,
+      'name': _name,
+      'sjsu_email': _email,
+      'password': _password,
+      'vehicleYear': _vehicleYear.toString(),
+      'vehicleMake': _vehicleMake,
+    };
+
+    print(
+        "name $_name and email $_email and password $_password and year $_vehicleYear and make $_vehicleMake and licence $_licence and model ");
+
+    String authority = "192.168.1.83:3000";
+    var url = Uri.http(authority, '/drivers');
+
+    http.put(url, body: body).then((response) {
+      if (response.statusCode == 200) {
+        // showDialog(context: context, builder: (BuildContext context) {
+        //   return showSignUpSuccessfully;}
+        // );
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return showSignUpSuccessfully;
+            });
+      } else {
+        // showDialog(context: context, builder: (BuildContext context) {
+        //   return showSignUpUnsuccessfully;}
+        // );
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return showSignUpUnsuccessfully;
+            });
+      }
+    }).catchError((e) {
+      print("Error $e");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Sign up form")),
+      appBar: AppBar(
+        title: const Text("Sign up form"),
+        centerTitle: true,
+      ),
       body: SingleChildScrollView(
         child: Form(
           key: _formKey,
@@ -51,6 +130,7 @@ class _DriverSignUpFormState extends State<SignUpFormDriver> {
             child: Column(
               children: <Widget>[
                 TextFormField(
+                  controller: _nameController,
                   decoration: const InputDecoration(labelText: 'Your name'),
                   //keyboardType: TextInputType.emailAddress,
                   validator: (value) {
@@ -62,6 +142,7 @@ class _DriverSignUpFormState extends State<SignUpFormDriver> {
                   onSaved: (value) => _name = value,
                 ),
                 TextFormField(
+                  controller: _vehicleMakeController,
                   decoration: const InputDecoration(labelText: 'Vehicle-Make'),
                   //keyboardType: TextInputType.emailAddress,
                   validator: (value) {
@@ -73,6 +154,7 @@ class _DriverSignUpFormState extends State<SignUpFormDriver> {
                   onSaved: (value) => _vehicleMake = value,
                 ),
                 TextFormField(
+                  controller: _vehicleYearController,
                   decoration: const InputDecoration(labelText: 'Vehicle-Year'),
                   validator: (value) {
                     if (value == null || value.isEmpty) {
@@ -83,8 +165,10 @@ class _DriverSignUpFormState extends State<SignUpFormDriver> {
                   onSaved: (value) => _vehicleYear = value as int?,
                 ),
                 TextFormField(
+                  controller: _licenceController,
                   decoration:
                       const InputDecoration(labelText: 'Licence plate number'),
+                  keyboardType: TextInputType.emailAddress,
                   validator: (value) {
                     if (value == null || value.isEmpty) {
                       return 'Please enter your Licence plate number';
@@ -106,6 +190,7 @@ class _DriverSignUpFormState extends State<SignUpFormDriver> {
                   onSaved: (value) => _email = value,
                 ),
                 TextFormField(
+                  controller: _passwordController,
                   decoration: const InputDecoration(labelText: 'Password'),
                   //keyboardType: TextInputType.visiblePassword,
                   validator: (value) {
@@ -121,7 +206,7 @@ class _DriverSignUpFormState extends State<SignUpFormDriver> {
                   onPressed: () {
                     // call both functions here
                     _validateEmail();
-                    _goback();
+                    _signUpDriver();
                   },
                   //onPressed: _validateEmail,
                   child: const Text('Complete'),
--- sharideapp/lib/screens/passengerSignupScreen.dart
+++ sharideapp/lib/screens/passengerSignupScreen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:http/http.dart' as http;
 
 class SignUpForm extends StatefulWidget {
   @override
@@ -6,13 +9,29 @@ class SignUpForm extends StatefulWidget {
 }
 
 class _SignUpFormState extends State<SignUpForm> {
+  AlertDialog showSignUpSuccessfully = const AlertDialog(
+            title: Text("Sign up successfully, please go back and log in"),
+            actions:[
+              // okButton,
+            ]
+  );
+
+  AlertDialog showSignUpUnsuccessfully = const AlertDialog(
+            title: Text("Sign up unsuccessfully"),
+            actions:[
+              // okButton,
+            ]
+  );
+
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _defaultLocationController = TextEditingController();
 
   String? _email;
   String? _password;
   String? _name;
-  String? _userName;
   String? _defaultLocation;
 
   void _validateEmail() {
@@ -22,6 +41,9 @@ class _SignUpFormState extends State<SignUpForm> {
     }
     setState(() {
       _email = _emailController.text;
+      _password = _passwordController.text;
+      _name = _nameController.text;
+      _defaultLocation = _defaultLocationController.text;
     });
   }
 
@@ -35,89 +57,103 @@ class _SignUpFormState extends State<SignUpForm> {
     });
   }
 
+  void _signUpPassenger() {
+    var body = {
+      'sjsu_email': _email,
+      'password': _password,
+      'address': _defaultLocation,
+      'name': _name,
+    
+    };
+
+    String authority = "192.168.1.83:3000";
+    var url = Uri.http(authority, '/customers');
+
+    http.put(url, body: body).then((response) {
+        if(response.statusCode == 200) {
+          showDialog(context: context, builder: (BuildContext context) {
+            return showSignUpSuccessfully;}
+          );
+        } else {
+          showDialog(context: context, builder: (BuildContext context) {
+            return showSignUpUnsuccessfully;}
+          );
+        }
+      }).catchError((e) {
+        print("Error");
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Sign up"),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Form(
-          key: _formKey,
-          child: Container(
-            padding: const EdgeInsets.all(15),
-            color: const Color.fromARGB(255, 255, 255, 255),
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Your name'),
-                  //keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your name';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) => _name = value,
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Username'),
-                  //keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your user name';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) => _userName = value,
-                ),
-                TextFormField(
-                  controller: _emailController,
-                  decoration: const InputDecoration(labelText: 'E-Mail'),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value == null || !value.endsWith('@sjsu.edu')) {
-                      return 'Only SJSU email addresses are allowed!';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) => _email = value,
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Password'),
-                  //keyboardType: TextInputType.visiblePassword,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    return null;
-                  },
-                  obscureText: true,
-                  onSaved: (value) => _password = value,
-                ),
-                TextFormField(
-                  decoration:
-                      const InputDecoration(labelText: 'Default location'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your default location';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) => _defaultLocation = value,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    // call both functions here
-                    _validateEmail();
-                    _goback();
-                  },
-                  //onPressed: _validateEmail,
-                  child: const Text('Complete'),
-                ),
-              ],
-            ),
+      appBar: AppBar(title: const Text("Sign up form")),
+      body: Form(
+        key: _formKey,
+        child: Container(
+          padding: const EdgeInsets.all(15),
+          color: const Color.fromARGB(255, 255, 255, 255),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(labelText: 'Your name'),
+                //keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your name';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _name = value,
+              ),
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(labelText: 'E-Mail'),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value == null || !value.endsWith('@sjsu.edu')) {
+                    return 'Only SJSU email addresses are allowed!';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _email = value,
+              ),
+              TextFormField(
+                controller: _passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                //keyboardType: TextInputType.visiblePassword,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+                obscureText: true,
+                onSaved: (value) => _password = value,
+              ),
+              TextFormField(
+                controller: _defaultLocationController,
+                decoration:
+                    const InputDecoration(labelText: 'Default location'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your default location';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _defaultLocation = value,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // call both functions here
+                  _validateEmail();
+                  _signUpPassenger();
+                },
+                //onPressed: _validateEmail,
+                child: const Text('Sign up'),
+              ),
+            ],
           ),
         ),
       ),
